7c15facea61988d8a80b5a78ed813064ff5cb297
Maycon Negrini <maycon128@hotmail.com>
2023-09-26 15:12:49 -0300

    create pinia store

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 
 import { createApp } from 'vue'
 
+import { createPinia } from 'pinia'
+
 import { registerPlugins } from '@/plugins'
 
 const app = createApp(App)
+
+app.use(createPinia())
 
 registerPlugins(app)
 
