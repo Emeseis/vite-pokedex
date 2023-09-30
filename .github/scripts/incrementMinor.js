1f732e6e001e624de519d44e4664f5366622c061
Maycon Negrini <maycon128@hotmail.com>
2023-09-30 14:32:39 -0300

    github action fix

--- .github/scripts/incrementMinor.js
+++ .github/scripts/incrementMinor.js
@@ -1,10 +1,10 @@
-import { incrementVersion } from "./incrementVersion";
+const { incrementVersion } = require("./incrementVersion");
 
-const incMinorVersion = (version) => {
+function incMinorVersion(version) {
   const [major, minor, patch] = version.split(".");
 
   if (isNaN(Number(minor))) throw Error(`Version is not made up of numbers! "${version}"`);
-  
+
   const newMinor = String(Number(minor) + 1);
 
   return [major, newMinor, patch].join(".");
--- .github/scripts/incrementVersion.js
+++ .github/scripts/incrementVersion.js
@@ -1,14 +1,15 @@
-import * as fs from 'fs';
+const fs = require("fs");
+const { getLatestVersion } = require("./getLatestVersion");
+const { saveVersion } = require("./saveVersion");
 
-import { getLatestVersion } from './getLatestVersion';
-import { saveVersion } from './saveVersion';
-
-export const incrementVersion = (incrementer) => {
+function incrementVersion(incrementer) {
   const rawPackage = fs.readFileSync("./package.json");
   const parsedPackage = JSON.parse(rawPackage);
   const latestVersion = getLatestVersion(parsedPackage);
   const nextVersion = incrementer(latestVersion);
   parsedPackage.version = nextVersion;
-  
+
   saveVersion(parsedPackage);
 };
+
+module.exports = { incrementVersion };
