dc07a4ab4bc48c67efdd81fc14fcb46e041b7e7e
Maycon Negrini <maycon128@hotmail.com>
2023-10-06 10:58:50 -0300

    scripts refactor

--- scripts/saveVersion.js
+++ scripts/saveVersion.js
@@ -1,24 +1,17 @@
 const fs = require('fs');
-const { gitAdd, gitCommit } = require('./simpleGit');
+const { spawnSync } = require('child_process');
 
 function saveVersion(packageJson) {
   const { version } = packageJson;
 
-  fs.writeFile(
-    './package.json',
+  fs.writeFile('./package.json',
     Buffer.from(JSON.stringify(packageJson, null, 2)),
     { encoding: 'utf8' },
     (err) => {
-      if (err) {
-        return console.log('Error!', err);
-      }
-
-      gitAdd();
-      gitCommit(`Published version: ${version}`);
-
-      return console.log(
-        `Successfully updated and committed package.json to version ${version}`
-      );
+      if (err) return console.log('Error!', err);
+      spawnSync('git', ['add', 'package.json']);
+      spawnSync('git', ['commit', `-m 'Published version: ${version}'`]);
+      return console.log(`Successfully updated and committed package.json to version ${version}`);
     }
   );
 }
